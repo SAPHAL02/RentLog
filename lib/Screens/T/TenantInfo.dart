4b51265b96737896d79a34c85ce87f6155e4f19f
Saphal02 <agarwalsaphal@gmail.com>
2023-06-05 19:22:36 +0530

    pdf fetch

--- lib/Screens/T/TenantInfo.dart
+++ lib/Screens/T/TenantInfo.dart
@@ -1,6 +1,10 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:path_provider/path_provider.dart';
 import '../../utils/color_util.dart';
 
 class TenantInfo extends StatefulWidget {
@@ -13,60 +17,149 @@ class TenantInfo extends StatefulWidget {
 }
 
 class _TenantInfoState extends State<TenantInfo> {
-  void showBill() {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      // Check if the room folder exists in Firebase Storage
-      Future<bool> doesFolderExist() async {
-        String folderPath = 'rooms/${widget.roomId}';
-        firebase_storage.Reference folderRef =
-            firebase_storage.FirebaseStorage.instance.ref().child(folderPath);
-        firebase_storage.ListResult listResult = await folderRef.list();
+  String? pdfFilePath; // Variable to hold the PDF file path
+  Stream<firebase_storage.ListResult>? stream; // Stream for listening to changes in the folder
 
-        return listResult.items.isNotEmpty;
+  void showBill() async {
+    bool doesFolderExist = await _checkFolderExistence();
+
+    if (doesFolderExist) {
+      String? filePath = await _findPDFFilePath();
+
+      if (filePath != null) {
+        setState(() {
+          pdfFilePath = filePath; // Store the PDF file path in the local variable
+        });
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Scaffold(
+              appBar: AppBar(title: const Text('PDF')),
+              body: Container(
+                padding: const EdgeInsets.all(16.0),
+                child: PDFView(filePath: pdfFilePath),
+              ),
+            ),
+          ),
+        );
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Error'),
+              content: const Text('The PDF file does not exist.'),
+              actions: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
       }
-
-      return FutureBuilder<bool>(
-        future: doesFolderExist(),
-        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            // Show a loading indicator while checking
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasData && snapshot.data!) {
-            // Room folder exists in Firebase Storage
-            return AlertDialog(
-              title: const Text('Room ID'),
-              content: Text('The current room ID is: ${widget.roomId}'),
-              actions: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('OK'),
-                ),
-              ],
-            );
-          } else {
-            // Room folder does not exist in Firebase Storage
-            return AlertDialog(
-              title: const Text('Room ID'),
-              content: const Text('The room folder does not exist.'),
-              actions: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('OK'),
-                ),
-              ],
-            );
-          }
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Room ID'),
+            content: const Text('The room folder does not exist.'),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
         },
       );
-    },
-  );
+    }
+  }
+
+  Future<bool> _checkFolderExistence() async {
+    String folderPath = 'rooms/${widget.roomId}';
+    firebase_storage.Reference folderRef =
+        firebase_storage.FirebaseStorage.instance.ref().child(folderPath);
+    firebase_storage.ListResult listResult = await folderRef.list();
+    
+    return listResult.items.isNotEmpty;
+  }
+
+  Future<String?> _findPDFFilePath() async {
+  String folderPath = 'rooms/${widget.roomId}';
+  firebase_storage.Reference folderRef =
+      firebase_storage.FirebaseStorage.instance.ref().child(folderPath);
+
+  firebase_storage.ListResult listResult = await folderRef.listAll();
+
+  for (var item in listResult.items) {
+    if (item.name == 'bill_${widget.roomId}.pdf') {
+      String filePath = await _downloadPDF(item);
+      return filePath;
+    }
+  }
+
+  return null;
 }
+
+
+  Future<String> _downloadPDF(firebase_storage.Reference storageRef) async {
+  Directory appDocDir = await getApplicationDocumentsDirectory();
+  String appDocPath = appDocDir.path;
+  String fileName = 'bill_${widget.roomId}_${DateTime.now().millisecondsSinceEpoch}.pdf';
+  String filePath = '$appDocPath/$fileName';
+
+  File file = File(filePath);
+  if (file.existsSync()) {
+    // If the file already exists, return the file path
+    return filePath;
+  }
+
+  await storageRef.writeToFile(file);
+
+  return filePath;
+}
+
+  Stream<firebase_storage.ListResult> _listenForChanges() {
+    String folderPath = 'rooms/${widget.roomId}';
+    firebase_storage.Reference folderRef =
+        firebase_storage.FirebaseStorage.instance.ref().child(folderPath);
+
+    return folderRef.list().asStream();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Start listening for changes when the widget is initialized
+    stream = _listenForChanges();
+    stream?.listen((firebase_storage.ListResult listResult) {
+      // Handle changes in the folder
+      // Update the PDF file path if a new bill is uploaded
+      setState(() {
+        for (var item in listResult.items) {
+          if (item.name == 'bill_${widget.roomId}.pdf') {
+            pdfFilePath = null; // Reset the PDF file path
+            break;
+          }
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // Cancel the stream subscription when the widget is disposed
+    stream?.drain();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
